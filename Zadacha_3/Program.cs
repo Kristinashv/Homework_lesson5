d85de48a5a4183f45d663d3f327300b7ca7c2c7d
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-05 02:14:41 +0300

    add Zadacha 3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Zadacha 3
+
+Console.WriteLine("Введите размер массива");
+    Console.Write("x = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("y = ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[,] arr = new int [n,m];
+    Random rand = new Random();
+    Console.WriteLine("Массив случайных чисел : ");
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < m ; j++)
+        {
+            arr[i,j] = rand.Next(10,100);
+            Console.Write(arr[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+    int min = 0;
+    for(int j = 0; j < m ; j++)
+    {
+        min += arr[0,j];
+    }
+    int num = 0;
+    for(int i = 0; i < n ; i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < m ; j++)
+        {
+            sum += arr[i,j];
+        }
+        if(min>sum)
+        {
+            min = sum;
+            num = i + 1;
+        }
+    }
+    Console.WriteLine("Строка с наименьшими числами - " + num + ", их сумма равна = " + min);
